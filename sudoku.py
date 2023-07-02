ec99a3000162d6a7afe2d6605d06b8e6c309ea41
Paul Duncan <pabs@pablotron.org>
2023-07-02 12:03:45 -0400

    ./sudoku.py: add example with solution and example without solution to module docstring

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,80 @@
-"""sudoku solver"""
+"""
+Sudoku Solver.
+
+Example: Find Solution
+----------------------
+Script:
+
+    import sudoku
+
+    # build grid with solution
+    grid = [
+      8, 0, 0,  0, 0, 0,  0, 0, 0,
+      0, 0, 3,  6, 0, 0,  0, 0, 0,
+      0, 7, 0,  0, 9, 0,  2, 0, 0,
+
+      0, 5, 0,  0, 0, 7,  0, 0, 0,
+      0, 0, 0,  0, 4, 5,  7, 0, 0,
+      0, 0, 0,  1, 0, 0,  0, 3, 0,
+
+      0, 0, 1,  0, 0, 0,  0, 6, 8,
+      0, 0, 8,  5, 0, 0,  0, 1, 0,
+      0, 9, 0,  0, 0, 0,  4, 0, 0
+    ]
+
+    # get solution
+    solution = sudoku.solve(grid)
+
+    # print result to standard output
+    print(sudoku.pair_to_string(grid, solution) if solution else 'no solution')
+
+Output:
+
+    -------------------------    -------------------------
+    | 8 - - | - - - | - - - |    | 8 1 2 | 7 5 3 | 6 4 9 |
+    | - - 3 | 6 - - | - - - |    | 9 4 3 | 6 8 2 | 1 7 5 |
+    | - 7 - | - 9 - | 2 - - |    | 6 7 5 | 4 9 1 | 2 8 3 |
+    |-----------------------|    |-----------------------|
+    | - 5 - | - - 7 | - - - |    | 1 5 4 | 2 3 7 | 8 9 6 |
+    | - - - | - 4 5 | 7 - - | -> | 3 6 9 | 8 4 5 | 7 2 1 |
+    | - - - | 1 - - | - 3 - |    | 2 8 7 | 1 6 9 | 5 3 4 |
+    |-----------------------|    |-----------------------|
+    | - - 1 | - - - | - 6 8 |    | 5 2 1 | 9 7 4 | 3 6 8 |
+    | - - 8 | 5 - - | - 1 - |    | 4 3 8 | 5 2 6 | 9 1 7 |
+    | - 9 - | - - - | 4 - - |    | 7 9 6 | 3 1 8 | 4 5 2 |
+    -------------------------    -------------------------
+
+Example: No Solution
+--------------------
+Script:
+
+    import sudoku
+
+    # build grid with no solution
+    grid = [
+      8, 8, 0,  0, 0, 0,  0, 0, 0,
+      0, 0, 3,  6, 0, 0,  0, 0, 0,
+      0, 7, 0,  0, 9, 0,  2, 0, 0,
+
+      0, 5, 0,  0, 0, 7,  0, 0, 0,
+      0, 0, 0,  0, 4, 5,  7, 0, 0,
+      0, 0, 0,  1, 0, 0,  0, 3, 0,
+
+      0, 0, 1,  0, 0, 0,  0, 6, 8,
+      0, 0, 8,  5, 0, 0,  0, 1, 0,
+      0, 9, 0,  0, 0, 0,  4, 0, 0
+    ]
+
+    # get solution
+    solution = sudoku.solve(grid)
+
+    # print grid and solution
+    print(sudoku.pair_to_string(grid, solution) if solution else 'no solution')
+
+Output:
+
+    no solution
+"""
 
 import z3
 
